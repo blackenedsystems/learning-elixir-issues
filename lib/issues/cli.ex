c8d9f8eb79e6e2caad41664c5100a5fd694e7c1a
blackenedsystems <alan@blackenedsystems.com>
2016-04-19 09:31:04 +0200

    add missing files

---
+++ lib/issues/cli.ex
@@ -0,0 +1,84 @@
+defmodule Issues.CLI do
+	
+	@default_count 4
+
+	@moduledoc """
+	Handle the command line parsing and the dispatch to 
+	the various functions that end up generating a 
+	table of the last _n_ issues in a github project.	
+	"""
+
+	def main(argv) do
+		argv
+		|> parse_args
+		|> process		
+	end
+
+	@doc """
+	'argv' can be -h or --help, which returns :help
+
+	Otherwise it is a github user name, project name and (optionally)
+	the number of entries to format.
+
+	Return a tuple of '{ user, project, count }', or ':help' if help was given.
+	"""
+	def parse_args(argv) do
+		parse = OptionParser.parse(argv, switches: [ help: :boolean],
+										 aliases: [ h: :help])
+
+		case parse do
+			{ [ help: true ], _, _ } -> :help
+
+			{ _, [ user, project, count], _ } -> {user, project, String.to_integer(count)}
+
+			{ _, [user, project ], _ } -> { user, project, @default_count }
+
+			_ -> :help
+		end
+	end
+
+	def process(:help) do
+		IO.puts """
+		usage: issues <user> <project> [ count | #{@default_count} ]	
+		"""	
+		System.halt(0)
+	end
+
+	def process({user, project, count}) do
+		Issues.GithubIssues.fetch(user, project)
+		|> decode_response
+		|> convert_to_list_of_maps
+		|> sort_into_ascending_order
+		|> Enum.take(count)
+		|> print_data
+	end
+
+	def decode_response( {:ok, body} ), do: body
+
+	def decode_response( {:error, error} ) do
+		{_, messsage} = List.keyfind(error, "messsage", 0)
+		IO.puts "Error fetching from github: #{messsage}"
+		System.halt(2)
+	end
+
+	def convert_to_list_of_maps(list) do
+		list
+		|> Enum.map(&Enum.into(&1, Map.new))
+	end
+
+	def sort_into_ascending_order(list_of_issues) do
+		Enum.sort list_of_issues, fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
+	end
+
+	def print_line(line) do
+		id = Map.get(line, "id", "Not Found")
+		created_at = Map.get(line, "created_at", "?")
+		title = Map.get(line, "title", "whatever")
+		IO.puts "#{id} #{created_at} #{title}"
+	end
+
+	def print_data(data) do
+		IO.puts "Id | Created | Title"
+		Enum.all? data, &(print_line(&1))
+	end
+end
